659bf20f31f804acf79ff501047adbe47965e0a3
Jeremy <jliuu@umich.edu>
2021-08-13 15:41:13 -0500

    swift setup

---
+++ Geometricize/Geometricize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Geometricize
+//
+//  Created by Jeremy Liu on 8/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Geometricize/Geometricize/GeometricizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricizeApp.swift
+//  Geometricize
+//
+//  Created by Jeremy Liu on 8/13/21.
+//
+
+import SwiftUI
+
+@main
+struct GeometricizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
