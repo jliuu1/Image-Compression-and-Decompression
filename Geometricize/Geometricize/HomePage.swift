2d864bcd0a52543e602fc5fd545f51b0d9e8d1bf
Jeremy <jliuu@umich.edu>
2021-08-13 16:02:36 -0500

    added views

---
+++ Geometricize/Geometricize/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  Geometricize
+//
+//  Created by Jeremy Liu on 8/13/21.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, BOOMER!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
